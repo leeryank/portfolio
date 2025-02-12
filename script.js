de54b650d9c198b496dab4b46a9532ffb9a30bcf
leeryank <leeryank1@gmail.com>
2025-02-12 17:13:54 -0500

    Add Smooth Scroll Animation

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ const observer = new IntersectionObserver((entries) => {
   const hiddenElements = document.querySelectorAll(".hidden");
   hiddenElements.forEach((el) => observer.observe(el));
   
+  document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        document.getElementById(targetId).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
